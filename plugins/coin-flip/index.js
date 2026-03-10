26424ac3b7fcbae2fcd7188cdff73be08f924252
SuperDev <supernova@supers0ft.us>
2026-03-10 01:09:27 -0500

    round 5.2

--- plugins/coin-flip/index.js
+++ plugins/coin-flip/index.js
@@ -1,18 +1,30 @@
-let template = "";
-
 export default {
   name: "Coin Flip",
   description: "Flip a coin",
   trigger: "flip",
 
-  init(ctx) {
-    template = ctx.template;
-  },
-
   async execute(args) {
     const result = Math.random() < 0.5 ? "heads" : "tails";
 
-    const html = template.replace(/\{\{result\}\}/g, result);
+    const html = `
+<div class="coin-flip-container">
+  <div class="coin-wrapper">
+    <div class="coin ${result}">
+      <div class="coin-side heads">
+        <span>HEADS</span>
+      </div>
+      <div class="coin-side tails">
+        <span>TAILS</span>
+      </div>
+    </div>
+  </div>
+  <div class="result-text">
+    <div class="result-label">Result:</div>
+    <div class="result-value ${result}">${result}</div>
+  </div>
+  <button class="flip-again" onclick="location.href='?q=!flip'">Flip Again</button>
+</div>
+    `;
 
     return {
       title: "Coin Flip",
